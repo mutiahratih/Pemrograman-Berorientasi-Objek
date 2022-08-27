bf7deb52e77beb7ebc537f9ff4cf1db1467cd76a
mutiahratih <mutiaratih77@gmail.com>
2022-08-27 21:28:25 +0700

    MyProject2
    Cetak 3 huruf depan

---
+++ MyProject2/src/myproject2/Project2.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject2;
+
+/**
+ *
+ * @author HP
+ */
+public class Project2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("MMM   MMM  ");
+        System.out.print("UUU   UUU  ");
+        System.out.println("TTTTTTTTTT");
+        System.out.print("MMMM MMMM  ");
+        System.out.print("UUU   UUU  ");
+        System.out.println(" TTTTTTTT");
+        System.out.print("MM  M  MM  ");
+        System.out.print("UUU   UUU  ");
+        System.out.println("    TT   ");
+        System.out.print("MM     MM  ");
+        System.out.print(" UU   UU   ");
+        System.out.println("    TT   ");
+        System.out.print("MM     MM  ");
+        System.out.print("   UUU     ");
+        System.out.println("    TT   ");
+
+    }
+    
+}
