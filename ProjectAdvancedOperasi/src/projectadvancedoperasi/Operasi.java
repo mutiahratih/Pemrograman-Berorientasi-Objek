8f41a09acf4e007f4a0f9d64fe4a6ccb1c997868
mutiahratih <mutiaratih77@gmail.com>
2022-09-27 18:21:57 +0700

    LKPD04

---
+++ ProjectAdvancedOperasi/src/projectadvancedoperasi/Operasi.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectadvancedoperasi;
+
+/**
+ *
+ * @author HP
+ */
+public class Operasi {
+    // PENJUMLAHAN //
+    int jumlahkan(int a, int b) {
+        return (a+b);
+    }
+    
+    int jumlahkan(int a, int b, int c) {
+        return (a+b+c);
+    }
+    
+    double jumlahkan(double a, double b) {
+        return (a+b);
+    }
+    
+    double jumlahkan(double a, double b, double c) {
+        return (a+b+c);
+    }
+    
+    
+    // PERKALIAN //
+    int kalikan(int x, int y) {
+        return (x*y);
+    }
+    int kalikan(int x, int y, int z) {
+        return (x*y*z);
+    }
+    double kalikan(int x, double y) {
+        return (x*y);
+    }
+    double kalikan(double y, int x) {
+        return (x*y);
+    }
+    double kalikan(double x, double y) {
+        return (x*y);
+    }
+    double kalikan(int x, int y, double z) {
+        return (x*y*z);
+    }
+    double kalikan(int x, double y, int z) {
+        return (x*y*z);
+    }
+    double kalikan(int x, double y, double z) {
+        return (x*y*z);
+    }
+    double kalikan(double x, int y, int z) {
+        return (x*y*z);
+    }
+    double kalikan(double x, int y, double z) {
+        return (x*y*z);
+    }
+    double kalikan(double x, double y, double z) {
+        return (x*y*z);
+    }
+}
---
+++ ProjectAdvancedOperasi/src/projectadvancedoperasi/ProjectAdvancedOperasi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectadvancedoperasi;
+
+/**
+ *
+ * @author HP
+ */
+public class ProjectAdvancedOperasi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Operasi A = new Operasi();
+        
+        System.out.println("a. " + A.jumlahkan(3, 4));
+        System.out.println("b. " + A.jumlahkan(4, 7, -9));
+        System.out.println("c. " + A.jumlahkan(3.4, -0.002, 0.12313));
+       
+    }
+    
+}
